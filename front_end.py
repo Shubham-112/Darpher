b6d23c921e6deaaab8061b3335a98816e2c3a50c
Shubham-112 <shubhamy609@gmail.com>
2017-05-13 04:13:33 +0530

    Darpher v1.8
    Added functionality of browsing file.

--- front_end.py
+++ front_end.py
@@ -1,29 +1,62 @@
 from tkinter import *
+from tkinter import messagebox
+from tkinter import filedialog
 import tut
 
 def encrypt_file():
-    tut.encrypt(tut.getKey(password.get()),filename.get())
-    l3.text = "Successfully Encrypted"
+    if len(filename.get())==0 and text.get("1.0",END)!="\n":
+        inp = tut.encrypt_text(tut.getKey(password.get()),text.get("1.0", END))
+        text.delete('1.0', END)
+        text.insert(END, inp)
+    elif text.get("1.0",END)=="\n" and len(filename.get())!=0:
+        tut.encrypt(tut.getKey(password.get()),filename.get())
+    elif len(filename.get())==0 and len(text.get("1.0",END))==0:
+        messagebox.showerror("Darpher v2.1", "No input for Encryption")
+        return 0
+    elif len(filename.get())==0 and text.get("1.0",END)=="\n":
+        messagebox.showerror("Darpher v2.1", "Multiple inputs for Encryption")
+        return 0
+    else:
+        messagebox.showerror("Darpher v2.1", "Unknown Error occured")
+        return 0
+    messagebox.showinfo("Darpher v2.1", "File Encrypted Successfully")
     e1.delete(0, END)
     e2.delete(0, END)
-
 
 def decrypt_file():
-    filename_str=str(filename.get())
-    tut.decrypt(tut.getKey(password.get()),filename_str)
-    l3.text= "Successfulle Decrypted"
+    if len(filename.get())==0 and text.get("1.0",END)!="\n":
+        inp = tut.decrypt_text(tut.getKey(password.get()),text.get("1.0", END))
+        print(inp)
+        text.delete('1.0', END)
+        text.insert(END, inp)
+    elif text.get("1.0",END)=="\n" and len(filename.get())!=0:
+        tut.decrypt(tut.getKey(password.get()),filename.get())
+    elif len(filename.get())==0 and len(text.get("1.0",END))==0:
+        messagebox.showerror("Darpher v2.1", "No input for Decryption")
+        return 0
+    elif len(filename.get())==0 and text.get("1.0",END)=="\n":
+        messagebox.showerror("Darpher v2.1", "Multiple inputs for Decryption")
+        return 0
+    else:
+        messagebox.showerror("Darpher v2.1", "Unknown Error occured")
+        return 0
+    messagebox.showinfo("Darpher v2.1", "File Decrypted Successfully")
     e1.delete(0, END)
     e2.delete(0, END)
+
+def browse_file():
+    y = filedialog.askopenfile(filetypes=[("Text Files", "*.txt"), ("All Files", "*.*")])
+    e1.insert(END, y.name)
 
 window = Tk()
 
-window.wm_title("Darpher v1.3")
+window.wm_title("Darpher v2.1")
 
 l1=Label(window, text="Filename: ")
 l1.grid(row=1, column=1,rowspan=2)
 
 l2=Label(window, text="Password: ")
-l2.grid(row=3, column=1)
+l2.grid(row=10, column=1)
 
 filename=StringVar()
 e1=Entry(window, textvariable=filename)
@@ -31,15 +64,24 @@ e1.grid(row=1, column=3)
 
 password=StringVar()
 e2=Entry(window, textvariable=password)
-e2.grid(row=3, column=3)
+e2.grid(row=10, column=3)
+
+text = Text(window, height=4, width=40, pady=2,font=('Lucida Console', 8))
+text.grid(row=4, column=1, columnspan=4)
 
 b1=Button(window, text="Encrypt", command=encrypt_file)
-b1.grid(row=1,column=6)
+b1.grid(row=10,column=4, columnspan=2)
 
 b2=Button(window, text="Decrypt", command=decrypt_file)
-b2.grid(row=3, column=6)
+b2.grid(row=11, column=4, columnspan=2)
 
-l3=Label(window, text="WELCOME TO DARPHER v1.3")
-l3.grid(row=5, column=2, columnspan=2)
+b3 = Button(window, text="Browse File", command=browse_file)
+b3.grid(row=1, column=4, columnspan=2)
+
+l3=Label(window, text="WELCOME TO DARPHER v2.1")
+l3.grid(row=12, column=2, columnspan=2)
+
+l4=Label(window, text="Message")
+l4.grid(row=3, column=1)
 
 window.mainloop()
--- tut.py
+++ tut.py
@@ -6,7 +6,7 @@ from Crypto import Random
 def encrypt(key, filename):
     chunksize = 64*1024
     basename = os.path.basename(filename)
-    outputFile = os.path.dirname(filename)+"(encrypted)"+ basename
+    outputFile = os.path.dirname(filename)+"/(encrypted)"+ basename
     filesize = str(os.path.getsize(filename)).zfill(16)
     IV = Random.new().read(16)
 
@@ -25,6 +25,45 @@ def encrypt(key, filename):
                     chunk += b' '*(16-(len(chunk)%16))
 
                 outfile.write(encryptor.encrypt(chunk))
+
+
+
+def encrypt_text(key, text):
+    chunksize= 64*1024
+    temp_file = "temp.txt"
+    with open(temp_file, 'w') as output:
+        output.write(text)
+    temp_path=os.path.abspath(temp_file)
+    basename = os.path.basename(temp_path)
+    outputFile = os.path.dirname(temp_path)+"\\(encrypted)"+basename
+    filesize = str(os.path.getsize(temp_path)).zfill(16)
+    IV = Random.new().read(16)
+
+    encryptor = AES.new(key, AES.MODE_CBC, IV)
+
+    with open(temp_path,'rb') as inFile:
+        with open(outputFile, 'wb') as outfile:
+            outfile.write(filesize.encode('utf-8'))
+            outfile.write(IV)
+            while True:
+                chunk = inFile.read(chunksize)
+
+                if len(chunk)==0:
+                    break
+                elif len(chunk)%16!=0:
+                    chunk += b' '*(16-(len(chunk)%16))
+
+                outfile.write(encryptor.encrypt(chunk))
+                print(encryptor.encrypt("i am here       "))
+                print(type(encryptor.encrypt("i am here       ")))
+
+    output = ' '
+    with open(outputFile, 'rb') as out:
+        output =out.read()
+    print(output)
+    #os.remove(outputFile)
+    #os.remove(temp_path)
+    return output
 
 
 def decrypt(key, filename):
@@ -47,6 +86,37 @@ def decrypt(key, filename):
 
                 outfile.write(decryptor.decrypt(chunk))
             outfile.truncate(filesize)
+
+def decrypt_text(key, text):
+    chunksize = 64*1024
+    temp_file = "temp.txt"
+    byte_text = bytes(text, 'utf-8')
+    temp_path = os.path.abspath(temp_file)
+    with open(temp_file,'wb') as temp:
+        temp.write(byte_text)
+    outputFile = os.path.dirname(temp_path) + "\\(decrypted)" + temp_file
+
+    with open(temp_path, 'rb') as infile:
+        filesize = int(infile.read(16))
+        IV = infile.read(16)
+
+        decryptor = AES.new(key,AES.MODE_CBC,IV)
+        with open(outputFile,'wb') as outfile:
+            while True:
+                chunk = infile.read(chunksize)
+
+                if len(chunk)==0:
+                    break
+                outfile.write(decryptor.decrypt(chunk))
+            outfile.truncate(filesize)
+
+    output = ' '
+    with open(outputFile,'r') as file:
+        output = file.read()
+    os.remove(outputFile)
+    os.remove(temp_path)
+    return output
+
 
 def getKey(password):
     hasher = SHA256.new(password.encode('utf-8'))
